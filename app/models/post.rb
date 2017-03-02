8acc55fe83806964cd5cc7c013b0bbb02e7239cf
dison  ruan <disonruan@disons-MacBook-Air.local>
2017-03-02 16:29:44 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum:10}
+  validates :summary, length: {maximum:50}
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait
+
+  def clickbait
+    cbait = ["Won't Believe", "Secret", "Top number", "Guess"]
+    if title && !cbait.any?{|bait|title.include?(bait)}
+      self.errors[:clickbait] << "NO CLICKBAIT!!!"
+    end
+  end
 end
